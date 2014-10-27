949cedd85c5ff3c9b68d2dc2e92b037fb4685311
alba garcia de la camacha selgas <albagc@iota23.aulas.gsyc.es>
2014-10-27 17:32:54 +0100

    Ejercicio 9, DocStrings pasados

--- client.py
+++ client.py
@@ -28,8 +28,8 @@ my_socket.connect((SERVER, PORT))
 
 print "Enviando: " + METHOD.upper() + " sip:" + LINE + " SIP/2.0\r\n\r\n"
 print "Enviando: " + str(EXPIRES)
-my_socket.send(METHOD.upper() + " sip:" + LINE + " SIP/2.0\r\n" + "Expires: "
-                + str(EXPIRES))
+LINE_EXPIRES = "Expires: " + str(EXPIRES) + '\r\n\r\n'
+my_socket.send(METHOD.upper() + " sip:" + LINE + " SIP/2.0\r\n" + LINE_EXPIRES)
 data = my_socket.recv(1024)
 
 print 'Recibido -- ', data
